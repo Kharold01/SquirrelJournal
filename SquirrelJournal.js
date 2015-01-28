7dd183e0a0b13b00aaa481b97137078d8dcb8612
Katie Harold <kharold@sjccs142.saintjoe.edu>
2015-01-28 10:41:39 -0500

    // Adding days to the Squirrel's journal

---
+++ SquirrelJournal.js
@@ -0,0 +1,35 @@
+
+
+
+// Katie Harold
+// Adding to the squirrel's journal
+// Jan 28
+
+var journal = [
+  {events: ["work", "touched tree", "pizza",
+            "running", "television"],
+    squirrel: false},
+                   
+  {events: ["work", "ice cream", "cauliflower",
+            "lasagna", "touched tree", "brushed teeth"],
+    squirrel: false},
+
+  {events: ["weekend", "cycling", "break",
+            "peanuts", "beer"],
+    squirrel: true},
+
+  {events: ["weekend", "television", "pasta",
+            "video games", "orange juice"],
+    squirrel: true},
+
+  {events: ["work", "touched tree", "sandwich",
+            "basketball", "television"],
+    squirrel: false},
+
+  {events: ["work", "cycling", "pizza"
+	    "telephone", "milk"],
+    squirrel: false},
+
+  /*and so on*/
+
+  ];  
